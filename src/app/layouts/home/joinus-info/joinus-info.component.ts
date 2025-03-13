5fb01d1bcc7a3fcbc0191a376b88dfc29cf69c5b
Oscar P <oparrilla@gmail.com>
2025-03-13 20:28:15 +0100

    Mejora maquetacion. Otros.

---
+++ src/app/layouts/home/joinus-info/joinus-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-joinus-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './joinus-info.component.html',
+  styleUrl: './joinus-info.component.scss'
+})
+export class JoinusInfoComponent {
+
+}
---
+++ src/app/layouts/mission-info/mission-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './mission-info.component.html',
+  styleUrl: './mission-info.component.scss'
+})
+export class MissionInfoComponent {
+
+}
